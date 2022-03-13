531ad6a09d7c951a3ac5fd4a022fb27a6c06d048
katrich1996 <sergey.katrich96@mail.ru>
2022-03-13 14:12:46 +0300

    Первое ДЗ от Сергея

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int age = 19;
+        if (age >= 18 && age < 21) {
+            System.out.println("Ты достаточно взрлослый, чтобы водить, но недостаточно взрослый, чтобы пить алкоголь");
+
+        }
+        if (age < 7 || age > 18) {
+            System.out.println("Я думаю ты не ходишь в школу ");
+        }
+
     }
 }
+
