9bfc9bb3a302be7e2722858b7f3e552328144d3e
katrich1996 <sergey.katrich96@mail.ru>
2022-03-13 14:56:34 +0300

    Первое ДЗ от Сергея

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 int age = 50;
-        System.out.println("ты можешь водить");
+if (age >= 18 && age < 21) {
+    System.out.println("Ты достаточно взрослый, чтобы водить, но недостаточно, чтобы пить алкоголь");
+}
     }
 }
